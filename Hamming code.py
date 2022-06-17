32d4613f0db59c7e2f61576a750402c9a73fc077
Lucas9017 <lucaslamping@gmail.com>
2022-06-17 15:07:34 +0200

    Update Hamming code.py

--- Hamming code.py
+++ Hamming code.py
@@ -4,7 +4,7 @@ class Matrix:
         self.kolommen = kolommen
     
     def isMatrix(self):
-        if isinstance(self,Matrix) and len(self.elementen) % self.kolommen == 0:
+        if isinstance(self,Matrix) and len(self.elementen) % self.kolommen == 0 and isinstance(self.kolommen, int):
             return True
         else:
             return False
@@ -16,64 +16,65 @@ class Matrix:
             raise TypeError('input is geen matrix')
 
     def __add__(self,other):
-        som = Matrix()
         if self.isMatrix() and other.isMatrix() and len(self.elementen) == len(other.elementen) and self.kolommen == other.kolommen:
+            som = self
             for i in range(0,len(self.elementen)):
-                som.elementen.append(self.elementen[i] + other.elementen[i])
-            som.kolommen = self.kolommen
+                som.elementen[i]= self.elementen[i] + other.elementen[i]
         else:
             raise TypeError('de matrices hebben niet de juiste afmetingen')
         return som
 
-    __radd__ = __add__
-
     def __sub__(self,other):
-        verschil = Matrix()
         if self.isMatrix() and other.isMatrix() and len(self.elementen) == len(other.elementen) and self.kolommen == other.kolommen:
+            verschil = self
             for i in range(0,len(self.elementen)):
-                verschil.elementen.append(self.elementen[i] - other.elementen[i])
-            verschil.kolommen = self.kolommen
+                verschil.elementen[i]= self.elementen[i] - other.elementen[i]
         else:
             raise TypeError('de matrices hebben niet de juiste afmetingen')
         return verschil
 
-    __rsub__ = __sub__  
-
     def __mul__(self,other):
-        product = Matrix()
         if self.isMatrix() and other.isMatrix():
-            m = self.elementen // self.kolommen
+            product = Matrix()
+            m = len(self.elementen) // self.kolommen #Aantal rijen self
             n = self.kolommen
             p = other.kolommen
-            if n == other.elementen // other.kolommen:
-                for i in range(0,m):
-                    for j in range(0,p):
+            if n == len(other.elementen) // other.kolommen: #Afmetingen kloppen dus de matrices kunnen met elkaar vermenigvuldigd worden
+                for i in range(1,m+1):
+                    for j in range(1,p+1):
                         resultaat = 0
-                        for k in range(0,n):
-                            resultaat += self.elementen[i*m+n] * other.elementen[n*m+j]
+                        for k in range(1,n+1):
+                            resultaat += self.elementen[(i-1)*n+k-1] * other.elementen[(k-1)*p+j-1]
                         product.elementen.append(resultaat)
-                product.kolommen = self.kolommen
+                product.kolommen = p
             else:
                 raise TypeError('de matrices hebben niet de juiste afmetingen')
+        else:
+            raise TypeError('De input zijn geen matrices')
         return product
 
-    __rmul__=__mul__
-
     def __eq__(self,other):
-        teller_s=self.a*other.b
-        teller_o=other.a*self.b
-        return teller_s==teller_o
+        if self.isMatrix() and other.isMatrix() and len(self.elementen) == len(other.elementen) and self.kolommen == other.kolommen:
+            for i in range(0, len(self.elementen)):
+                if self.elementen[i] != other.elementen[i]:
+                    return False
+            return True
+        else:
+            return False
 
     def __ne__(self,other):
-        teller_s=self.a*other.b
-        teller_o=other.a*self.b
-        return teller_s!=teller_o
+        if self == other:
+            return False
+        else:
+            return True
+        
+    def __pos__(self):
+        if self.isMatrix():
+            return self
 
     def __neg__(self):
-        negatie=Breuk()
-        negatie.a=-self.a
-        negatie.b=self.b
-        return negatie
-
-    def __pos__(self):
-        return self
+        if self.isMatrix():
+            negatie = self
+            for i in range(0, len(self.elementen)):
+                negatie.elementen[i] = -1 * self.elementen[i]
+            return negatie
